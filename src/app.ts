aa2af7e155a1bd047690230222225afc673a21fb
Agustin Reseco Alonso <149005541+agustinresecodev@users.noreply.github.com>
2024-03-06 17:45:04 +0100

    app.ts, server.ts and data-source.ts configuration

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Application } from "express";
+import cors from "cors";
+import { corsOptions } from "./config/cors";
+import dotenv from "dotenv";
+//import apiRoutes from "./routes/api.routes"; 
+//import BaseRoutes from "./routes/base.routes";
+// -----------------------------------------------------------------------------
+
+dotenv.config();
+
+const app: Application = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors(corsOptions));
+
+//Register Base Routes
+//app.use('/',BaseRoutes); 
+
+//Register API Routes
+//app.use('/api',apiRoutes);
+
+
+
+export default app;
