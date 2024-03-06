5ff03f1d01703d18c48dafd64eda84758c61709e
Agustin Reseco Alonso <149005541+agustinresecodev@users.noreply.github.com>
2024-03-06 18:35:24 +0100

    migrations first implementantion

---
+++ src/database/migrations/1709746133244-CreateCommentsTable.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner,Table } from "typeorm";
+
+export class CreateCommentsTable1709746133244 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+               name: "comments",
+               columns: [
+                  {
+                     name: "id",
+                     type: "int",
+                     isPrimary: true,
+                     isGenerated: true,
+                     generationStrategy: "increment",
+                  },
+                  {
+                     name: "text",
+                     type: "varchar",
+                     length: "255",
+                  },
+                  {
+                    name: "valoration",
+                    type: "int"
+                  },
+                  {
+                    name: "user_id",
+                    type: "int",
+                  },
+                  {
+                    name: "business_id",
+                    type: "int",
+                  }
+                ],
+                foreignKeys: [
+                {
+                   columnNames: ["user_id"],
+                   referencedTableName: "users",
+                   referencedColumnNames: ["id"],
+                },
+                {
+                    columnNames: ["business_id"],
+                    referencedTableName: "businesses",
+                    referencedColumnNames: ["id"],
+                }
+                ],
+            }),
+            true
+         );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("comments");
+    }
+
+}
