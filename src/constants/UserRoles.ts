7c8fc110e597f068955ddf4fff348717a5bdcd6e
Agustin Reseco Alonso <149005541+agustinresecodev@users.noreply.github.com>
2024-03-18 16:03:21 +0100

    created Constant UserRoles for seeders

---
+++ src/constants/UserRoles.ts
@@ -0,0 +1,20 @@
+import { Role } from "../models/Role";
+
+export const UserRoles = {
+    ADMIN: {
+        id: 1,
+        name: "ADMIN",
+        description: "Administrator",
+
+    },
+    USER: {
+        id: 2,
+        name: "USER",
+        description: "User",
+    },
+    BUSINESS: {
+        id: 3,
+        name: "BUSINESS",
+        description: "Business",
+    }
+}
